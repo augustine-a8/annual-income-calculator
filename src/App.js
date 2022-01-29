eba42c7e66280fa90d0ba498226c8ccf5f3804da
augustine-a8 <logos.agbeko@gmail.com>
2022-01-29 11:23:45 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
-function App() {
+export default function App() {
+  const [workTime, setWorkTime] = useState({
+    hoursPerWeek: '',
+    weeksPerYear: '',
+  });
+  const [hourlyWage, setHourlyWage] = useState('');
+  const [annualIncome, setAnnualIncome] = useState('5');
+
+  function formatHourlyWage() {
+    return `$${hourlyWage}`;
+  }
+
+  function validateInput() {
+    if (
+      workTime.hoursPerWeek === '' ||
+      workTime.weeksPerYear === '' ||
+      hourlyWage === ''
+    ) {
+      alert('Missing Inputs');
+      return false;
+    }
+
+    return true;
+  }
+
+  function buttonClickHandler() {
+    if (annualIncome !== '') {
+      setWorkTime({
+        hoursPerWeek: '',
+        weeksPerYear: '',
+      });
+      setHourlyWage('');
+      setAnnualIncome('');
+
+      return;
+    }
+    const result = validateInput();
+    result &&
+      setAnnualIncome(
+        `$${workTime.hoursPerWeek * workTime.weeksPerYear * hourlyWage}`
+      );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="incomeGenerator">
+        <div className="header">
+          <h2>Annual Income Calculator</h2>
+          <button onClick={() => buttonClickHandler()}>
+            {annualIncome ? 'CLEAR' : 'CALCULATE'}
+          </button>
+        </div>
+        <div className="body">
+          <div className="workingTime__section">
+            <div className="workingTime__section-input">
+              <label htmlFor="">Working hours per week: </label>
+              <input
+                type="text"
+                value={workTime.hoursPerWeek}
+                onChange={(e) =>
+                  setWorkTime({
+                    ...workTime,
+                    hoursPerWeek: e.target.value,
+                  })
+                }
+              />
+            </div>
+            <div className="workingTime__section-input">
+              <label htmlFor="">Working weeks per year: </label>
+              <input
+                type="text"
+                value={workTime.weeksPerYear}
+                onChange={(e) =>
+                  setWorkTime({
+                    ...workTime,
+                    weeksPerYear: e.target.value,
+                  })
+                }
+              />
+            </div>
+            <div className="workingTime__section-input">
+              <label htmlFor="">Hourly wage : </label>
+              <input
+                type="text"
+                value={hourlyWage}
+                onChange={(e) => setHourlyWage(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="wage__section">
+            <div className="wage__section-input">
+              <label htmlFor="">Annual Income: </label>
+              <p>{annualIncome}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
